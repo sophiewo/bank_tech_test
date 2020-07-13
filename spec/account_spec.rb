6aa501aa531431b9656ee0f40312f94182848d0d
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-07-13 12:00:44 +0100

    Start account balance at £0

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,6 +3,9 @@ require 'account'
 describe Account do
   it 'exists' do
     expect(Account).to respond_to(:new)
-    
+  end
+
+  it 'account starts with a balance of £0' do
+    expect(subject.account_balance).to eq(0)
   end
 end
