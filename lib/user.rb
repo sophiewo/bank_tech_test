d8dab59a48bf46e0046419dc00dea9d180c39c78
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-07-31 10:36:11 +0100

    TDD first User Class, test passing

---
+++ lib/user.rb
@@ -0,0 +1,6 @@
+class User
+
+  def show_user_balance
+    0
+  end
+end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,9 +2,9 @@ require 'user'
 
 describe User do
 
-  describe '#show_balance' do
+  describe '#show_user_balance' do
     it 'shows the user their balance' do
-      expect(subject.show_balance).to eq(0)
+      expect(subject.show_user_balance).to eq(0)
     end
   end
 end
