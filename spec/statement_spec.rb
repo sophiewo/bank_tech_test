5a521e86c8f1983f1b7ffabd5227806cc8c75cc8
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-07-31 12:22:06 +0100

    Fixes statement formatter test

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -3,14 +3,14 @@ describe Statement do
     expect(Statement).to respond_to(:new)
   end
 
-  describe '#print' do
-    it "returns the users statement" do
+  describe '#statement_formatter' do
+    it "provides format for users statement" do
       account_double = double [{:date=>"30/07/2020", :credit=>20, :debit=>"       ", :account_balance=>20}]
 
       account_class_double = double :account_class, new: account_double
        
       # account_statement = [{:date=>"30/07/2020", :credit=>20, :debit=>"       ", :account_balance=>20}]
-      expect(subject.print).to start_with('date       || credit || debit || balance')
+      expect { subject.statement_formatter }.to output {'date       || credit || debit || balance' }.to_stdout
     end
   end
 end
