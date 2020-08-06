f09e9a70b8df8dc89b93ef03abd5843f4f1b42e3
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-08-06 09:01:42 +0100

    Amends statement_formatter to comply with requirements, needs further editing

--- lib/statement.rb
+++ lib/statement.rb
@@ -3,9 +3,9 @@ class Statement
   attr_reader :account_statement
 
   def statement_formatter(account_statement)
-    puts 'date       || credit || debit || balance'
+    puts 'date || credit || debit || balance'
     account_statement.each do |hash|
-      puts "#{hash[:date]} ||      #{hash[:credit]}||     #{hash[:debit]} ||    #{hash[:account_balance]}"
+      puts "#{hash[:date]} || #{hash[:credit]}.00|| #{hash[:debit]}.00 ||#{hash[:account_balance]}.00"
     end
   end
 end
